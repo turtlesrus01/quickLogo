1f5c4686c6712d38dec2c7ecb525955112dd2e86
Green Camper <stefan.palacios@gmail.com>
2023-03-31 23:26:09 -0500

    Started pseudocode

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+//function for inquirer prompts 
+//prompt for Logo text (up to 3 chars)
+//prompt for color keyword or hex code 
+//prompt for which shape (circle, triangle, and square)
