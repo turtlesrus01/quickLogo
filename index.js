f053766819d47fbff1e4fbc6ac653202a3507da0
Green Camper <stefan.palacios@gmail.com>
2023-04-01 00:14:16 -0500

    Started inquirer function

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 //function for inquirer prompts 
-//prompt for Logo text (up to 3 chars)
+inquirer.prompt([
+  //prompt for Logo text (up to 3 chars)
+  {
+    type: 'input',
+    message: 'What is your user name?',
+    name: 'username',
+  },
+])
+.then((response) => {
+
+});
+
 //prompt for color keyword or hex code 
 //prompt for which shape (circle, triangle, and square)
+//function for SVG generation 
+//output file called logo.svg
+//largest size is a 300x300 image
+
