28595bf14d54e5e381ccd40ef0d343078c42829a
Green Camper <stefan.palacios@gmail.com>
2023-04-04 20:57:58 -0500

    Added the last SVG.js calls, changed module init to ES6, created a switch statement for shape selection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const { SVG, extend: SVGextend, Element : SVGElement} = require('@svgdotjs/svg.js');
+import fs from 'fs';
+import inquirer from 'inquirer';
+import { JSDOM } from 'jsdom';
+import * as d3 from 'd3';
+import { SVG, extend as SVGextend, Element as SVGElement } from '@svgdotjs/svg.js'
+
+
+
 
 //function for inquirer prompts 
 inquirer.prompt([
@@ -42,13 +47,13 @@ inquirer.prompt([
   //prompt for color keyword or hex code for shape
   {
     type: 'input',
-    message: 'Please type a color for the text:',
-    name: 'logoColor',
+    message: 'Please type a color for the shape:',
+    name: 'shapeColor',
     //validate method to check if entry is a valid hex code
-    validate: (logoColor) => {
+    validate: (shapeColor) => {
       //Regular expression to validate hex code entry
       const hex = new RegExp('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$');
-      if (!hex.test(logoColor)) {
+      if (!hex.test(shapeColor)) {
         return false;
       } 
       return true;
@@ -61,20 +66,42 @@ svgBuilder(response)
 })
 .catch((err) => console.log(err));
 
+//init dom from jsdom
+const Dom = new JSDOM(`<!DOCTYPE html><body></body>`);
+
 //function for SVG generation 
   //output file called logo.svg
   //largest size is a 300x300 image
 function svgBuilder(response) {
   //init variables 
   const prompts = response;
-  const draw = SVG();
+  const body = d3.select(Dom.window.document.querySelector('body'));
+  let draw = SVG().addTo(body);
   const logoBatch = draw.group();
   //call for creating the document
   logoBatch.size('300px', '300px').fill('white');
-  //svg call for logoText
-  logoBatch.rect(100, 100).move(100, 50).fill(prompts.logoColor)
-  //svg call for text color selection
-  //svg call for shape selection
-  //svg call for shape color selection
+  //svg call for shape selection and shape color selection
+  switch (prompts.shapes) {
+    case 'square':
+    logoBatch.rect(100, 100).move(100, 50).fill(prompts.textColor)
+    break;
+    case 'triangle':
+    logoBatch.rect().move(100, 50).fill(prompts.textColor)
+    break;
+    case 'circle':
+    logoBatch.circle(100).move(100, 50).fill(prompts.textColor)
+    break;
+  }
+
+  //svg call for text and text color selection 
+  logoBatch.text(prompts.logoText)
+  .font({size: 80, anchor: 'middle', leading:1.2})
+  .fill(prompts.textColor)
+  .move(150,140);
+
+  //Send batch text to draw element
+  draw.append(logoBatch);
   //filesystem call for svg file creation
+  const svgStr = draw.svg();
+  fs.writeFileSync('logo.svg', svgStr);
 }
